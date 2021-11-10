8e3eb8f120e4e26523e64591a9c397125692d9e3
Elaine <71743181+elainechan01@users.noreply.github.com>
2021-11-10 10:54:21 -0500

    Delete react-frontend-main/react-frontend-main directory

--- react-frontend-main/react-frontend-main/src/components/CarouselCard.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function CarouselCard(props) {
-  return (
-    <div class="carousel-card">
-      <img src={props.link} alt="" />
-    </div>
-  );
-}
--- react-frontend-main/react-frontend-main/src/components/SlideCard.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function SlideCard(props) {
-  return (
-    <div class="slide-card">
-      <img src={props.link} alt="" />
-    </div>
-  );
-}
