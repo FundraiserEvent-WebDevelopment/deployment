9fa786d5cb5da25416a98fb6549261a8065e4009
elainechan01 <elaineyunru.chan@wmich.edu>
2021-11-18 23:19:18 -0500

    update vendorcard

--- client/src/components/VendorCard.js
+++ client/src/components/VendorCard.js
@@ -36,7 +36,7 @@ export default function VendorCard(props) {
     async function getImages() {
       try {
         const response = await fetch(
-          `https://wmu-back2back.herokuapp.com/vendorimages/${props.id}`
+          `https://cors-anywhere.herokuapp.com/https://wmu-back2back.herokuapp.com/vendorimages/${props.id}`
         );
         const json = await response.json();
         setImages([...json]);
